90cafce7356d3eff6ec13e97fd9be03b9da8200c
singsl <95468712+singsl@users.noreply.github.com>
2022-09-22 19:07:27 +0800

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def get_weather():
 
 def get_count():
   delta = today - datetime.strptime(start_date, "%Y-%m-%d")
-  print(delta)
+  print(delta)print("得到")
   return delta.days
 
 def get_birthday():
