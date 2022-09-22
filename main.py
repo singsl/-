1fac09ff88c193c0f1744904c1f7f12b950aa9c1
singsl <95468712+singsl@users.noreply.github.com>
2022-09-22 19:07:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def get_count():
 
 def get_birthday():
   next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
-  print(next)
+  print(next)print("sdasdasdasdasdasfzv")
   if next < datetime.now():
     next = next.replace(year=next.year + 1)
   return (next - today).days
